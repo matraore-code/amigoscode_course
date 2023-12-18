ee4ab93b84f1ede3231a00cb85a900b29d9ac387
matraore-code <mamadou.moctartra@gmail.com>
2023-12-18 01:26:47 +0100

    basic calculus

--- Java/src/com/moctar/Main.java
+++ Java/src/com/moctar/Main.java
@@ -3,6 +3,48 @@ package com.moctar;
 public class Main {
 
     public static void main(String[] args) {
+       /*
         System.out.println("ReBonjour ");
+        System.out.println(10 + 10);
+
+        */
+
+        //primitives
+        int number1 = 20;
+        int number2 = 3;
+        int result = number2 + number1;
+        //System.out.println(result);
+
+        double pi = 3.14;
+        boolean isAdult = false;
+        char a = 'a';
+        byte  b = 123;
+        short sh = 234;
+        System.out.println("--------------------");
+
+        //Arithmetic operator
+        System.out.println(number2 + number1);
+        System.out.println(number2 - number1);
+        System.out.println(number2 / number1);
+        System.out.println(number2 * number1);
+        System.out.println(number2 % number1);
+        System.out.println("--------------------");
+
+        //increment - decrement
+        int num = 0;
+        int num2 = 0;
+        int increment = number1++;
+        int decrement = number2--;
+        System.out.println(increment);
+        System.out.println(decrement);
+        System.out.println(++num);
+        System.out.println(--num2);
+
+        System.out.println("--------------------");
+
+
+
+
+
     }
 }
